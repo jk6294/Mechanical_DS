9e96bf48542b810cc69daa214df7d02a91a194e5
Jason <jinsu1@seas.upenn.edu>
2019-05-05 21:43:08 -0400

    Revised letter N

--- figure4.m
+++ figure4.m
@@ -22,7 +22,7 @@ for i = 1:length(colBal)
 end
 
 % Figure Axis Bounds
-axM = [-1.4 1.4 -0.8 2];
+axM = [-1.5 1.5 -0.8 2];
 labX = -.1;
 labY = 0.90;
 labColY = 1.3;
@@ -40,7 +40,7 @@ cTr3 = [015 082 186]/255;
 
 % Module Parameters
 l1 = [0;-s]; l2 = [0;-1.5*s];
-a1 = 17; a2 = 38; a3 = 60;
+a1 = 19.5; a2 = 38; a3 = 60;
 R1 = rotz(-a1/2); R2 = rotz(-a2/2); R3 = rotz(-a3/2);
 R1 = R1(1:2,1:2); R2 = R2(1:2,1:2); R3 = R3(1:2,1:2);
 
@@ -51,8 +51,8 @@ subplot(NRow,NCol,cellM{1}); cla;
 Xs10 = [-s/2  0    s/2;...
         -0.5  1.0 -0.5];
 Xs1T = [R1*l2 [0;0] R1\l2] + [0;1];
-Xu1 = [-1.4  1.4;...
-        3/3  3/3];
+Xu1 = [-1.5  1.5;...
+        1.5  1.5];
 conn1 = [1 4; 2 4; 3 4; 1 5; 2 5; 3 5];
 [Xu1,fV] = construct_network(Xs10,Xs1T,Xu1,conn1,0,1);
 Xu1 = Xu1(1:2,:);
@@ -60,13 +60,14 @@ Xu1 = Xu1(1:2,:);
 Xs1f = [Xs10(1,:); -Xs10(2,:)+.5];
 Xu1f = [Xu1(1,:); -Xu1(2,:)+.5];
 % Show Network
+% visualize_conic_finite(Xs10,Xs1T,[-2 2;-2 2],[100;100],0,1,1);
 visualize_network(Xs10,Xu1,conn1,1,[255 100 100]/255, cTr1);
-axis(axM);
+% axis(axM);
 text(labX,labY,'a','Units','Normalized','fontsize',10,'fontweight','bold');
 
 subplot(NRow,NCol,cellM{5}); cla;
 % Simulate Network
-[XC1,fC] = sim_motion(Xs10,Xu1,conn1,.01,200,-[Xs10 Xu1],0);
+[XC1,fC] = sim_motion(Xs10,Xu1,conn1,.01,220,-[Xs10 Xu1],0);
 d1 = sqrt(squeeze(sum((XC1(:,1,:)-XC1(:,2,:)).^2)));
 d2 = sqrt(squeeze(sum((XC1(:,2,:)-XC1(:,3,:)).^2)));
 d3 = sqrt(squeeze(sum((XC1(:,1,:)-XC1(:,3,:)).^2)));
@@ -76,8 +77,9 @@ distL = sum(abs(distv - [d1 d2 d3]),2);
 pInd = find(distL==min(distL),1);
 visualize_network(XC1(:,1:3,pInd),XC1(:,4:5,pInd),conn1,...
                   1,[255 100 100]/255, cTr1);
-plot(d1); hold on; plot(d3); hold off;
-% axis(axM);
+% plot(d1); hold on; plot(d2); plot(d3); hold off;
+axis(axM);
+drawnow;
 
 
 
@@ -113,8 +115,9 @@ distL = sum(abs(distv - [d1 d2 d3]),2);
 pInd = find(distL==min(distL),1);
 visualize_network(XC2(:,1:3,pInd),XC2(:,4:5,pInd),conn1,...
                   1,[255 100 100]/255, cTr2);
-plot(d1); hold on; plot(d3); hold off;
-% axis(axM);
+% plot(d1); hold on; plot(d3); hold off;
+axis(axM);
+drawnow;
 
 
 %% c: Increase Distance
@@ -148,28 +151,33 @@ distL = sum(abs(distv - [d1 d2 d3]),2);
 pInd = find(distL==min(distL),1);
 visualize_network(XC3(:,1:3,pInd),XC3(:,4:5,pInd),conn1,...
                   1,[255 100 100]/255, cTr3);
-plot(d1); hold on; plot(d3); hold off;
-% axis(axM);
+% plot(d1); hold on; plot(d3); hold off;
+axis(axM);
+drawnow;
 
 
 %% d: Example Line
-subplot(NRow,NCol,cellM{9}); cla;
 % Collect Unspecified Node Posiitons
 XuC = {Xu1,Xu2,Xu3,Xu1f,Xu2f,Xu3f};
+C_UNC = [cTr1; cTr2; cTr3];
 % Create Chain of Specified Nodes
-nS4 = 46;
+nS4 = 56;
 Xs4L1 = s/2*ones(1,nS4+2);
 Xs4L2 = repmat([-.5 1],1,(nS4+2)/2);
 Xs4 = [cumsum(Xs4L1)-s; Xs4L2];
 % Decide Placement Order of Unspecified Nodes
-Xu4L = [2 2, 2 2, 2 2, 2 2, 2 2, 2 2,...
-        1 3, 1 3, 1 3,...
-        2 2, 2 2, 2 2, 2 2, 2 2,...
-        3 1 ,3 1, 3 1,...
-        2 2, 2 2, 2 2, 2 2, 2 2, 2 2];
+Xu4L = [2 2, 2 2, 2 2, 2 2, 2 2, 2 2, 2 2,...
+        1 3, 1 3, 1 3, 1 3,...
+        2 2, 2 2, 2 2, 2 2, 2 2, 2 2,...
+        3 1 ,3 1, 3 1, 3 1,...
+        2 2, 2 2, 2 2, 2 2, 2 2, 2 2, 2 2];
 Xu4 = zeros(2,2*nS4);
+C_UN4 = zeros(2*nS4,4);
 for i = 1:nS4
+    % Node Positions
     Xu4(:,[1,2]+2*(i-1)) = XuC{Xu4L(i)+3*mod(i-1,2)}+[s/2;0]*(i-1);
+    % Colors
+    C_UN4([1,2]+2*(i-1),:) = repmat([C_UNC(Xu4L(i),:) 1],2,1);
 end
 % Connections
 conn4s = repmat([3:nS4],6,1);
@@ -177,45 +185,64 @@ conn4s = [1;1;2;2;2;2;conn4s(:);repmat(nS4+1,4,1);repmat(nS4+2,2,1)];
 conn4u = repmat([1:6]',1,nS4+2)+[2:2:2*nS4+4]-6;
 conn4u = conn4u(:); conn4u = conn4u(conn4u>0 & conn4u<=size(Xu4,2))+nS4+2;
 conn4 = [conn4s conn4u];
-% Visualize
-visualize_network(Xs4,Xu4,conn4);
 
 
 %% Simulate
-[XC,fC] = sim_motion(Xs4,Xu4,conn4,.1,1200,-[Xs4,Xu4],0);
+[XC,fC] = sim_motion(Xs4,Xu4,conn4,.5,300,-[Xs4,Xu4],0);
+
+
+%% d: Folded Network
+subplot(NRow,NCol,cellM{9}); cla;
+% Visualize
+visualize_network(Xs4,Xu4,conn4,.3,[255 100 100]/255,C_UN4);
+da = mean(squeeze(sqrt(sum(diff(XC(:,1:nS4+2,:),1,2).^2))) + l2(2));
+pInd = find(abs(da) == min(abs(da)),1);
+RV = rotz(7); RV = RV(1:2,1:2);
+XSh = 42; YSh = -3;
+visualize_network(RV*XC(:,1:nS4+2,round(pInd/2))+[XSh;YSh],...
+                  RV*XC(:,[1:2*nS4]+nS4+2,round(pInd/2))+[XSh;YSh],conn4,...
+                  .3,[255 100 100]/255,C_UN4);
+XSh = 70; YSh = -3;
+visualize_network(RV*XC(:,1:nS4+2,pInd)+[XSh;YSh],...
+                  RV*XC(:,[1:2*nS4]+nS4+2,pInd)+[XSh;YSh],conn4,.3,...
+                  [255 100 100]/255,C_UN4);
+axis([-1,102,-10,10]);
+
+
+%% 
 
 
 
 %% Animate Network Alone
-fig = figure(5); clf;
-fName = 'animation_net.gif';
-dT = 0.03;
-nSV = 1;
-Xs1a = Xs4;
-Xu1a = Xu4;
-conn1a = conn4;
-XdotV = XC(:,1:size(Xs1a,2),1); XdotV = XdotV-mean(XdotV,2);
-
-for i = 1:10:901
-    cla;
-    visualize_network(XC(:,1:size(Xs1a,2),i),...
-                      XC(:,[1:size(Xu1a,2)]+size(Xs1a,2),i),conn1a,1);
-    axis([min(min(XC(1,:,:))) max(max(XC(1,:,:))) min(min(XC(2,:,:))) max(max(XC(2,:,:)))]);
-    set(gca,'visible',0);
-    drawnow;
-
-    % Capture the plot as an image
-    frame = getframe(fig);
-    im = frame2im(frame);
-    [imind,cm] = rgb2ind(im,256);
-    % Write to the GIF File
-    if nSV == 1
-      imwrite(imind,cm,fName,'gif', 'Loopcount',inf,'DelayTime',dT);
-      nSV = 0;
-    else
-      imwrite(imind,cm,fName,'gif','WriteMode','append','DelayTime',dT);
-    end
-end
+% fig = figure(5); clf;
+% fName = 'animation_net.gif';
+% dT = 0.03;
+% nSV = 1;
+% Xs1a = Xs4;
+% Xu1a = Xu4;
+% conn1a = conn4;
+% XdotV = XC(:,1:size(Xs1a,2),1); XdotV = XdotV-mean(XdotV,2);
+% 
+% for i = 1:3:size(XC,3)-10
+%     cla;
+%     visualize_network(XC(:,1:size(Xs1a,2),i),...
+%                       XC(:,[1:size(Xu1a,2)]+size(Xs1a,2),i),conn1a,.5);
+%     axis([min(min(XC(1,:,:))) max(max(XC(1,:,:))) min(min(XC(2,:,:))) max(max(XC(2,:,:)))]);
+%     set(gca,'visible',0);
+%     drawnow;
+% 
+%     % Capture the plot as an image
+%     frame = getframe(fig);
+%     im = frame2im(frame);
+%     [imind,cm] = rgb2ind(im,256);
+%     % Write to the GIF File
+%     if nSV == 1
+%       imwrite(imind,cm,fName,'gif', 'Loopcount',inf,'DelayTime',dT);
+%       nSV = 0;
+%     else
+%       imwrite(imind,cm,fName,'gif','WriteMode','append','DelayTime',dT);
+%     end
+% end
 
 
 %% Size and Save Figure
--- visualize_network.m
+++ visualize_network.m
@@ -6,20 +6,25 @@ function [] = visualize_network(Xs, Xu, conn, sC, C_SN, C_UN)
 % Xu:       d x m  matrix of unspecified node positions
 % conn:     k x 2  matrix of edges from node i to node j
 % sC:       1 x 1  optional scalar for scaling network
-% eC:       1 x 4  optional vector of colors
+% C_SN:     n x 4  optional vector of colors
+% C_UN:     m x 4  optional vector of colors
 %
 % Outputs
 % Figure. Assumes the desired figure placement is already selected
 
 if(nargin == 3)
     sC = 1;
-    C_SN = [255 100 100]/255;         % Color of Specified Node
-    C_UN = [100 100 255]/255;
+    C_SN = repmat([255 100 100]/255,size(Xs,2),1); 
+    C_UN = repmat([100 100 255]/255,size(Xu,2),1);
 elseif(nargin == 4)
-    C_SN = [255 100 100]/255;         % Color of Specified Node
-    C_UN = [100 100 255]/255;
+    C_SN = repmat([255 100 100]/255,size(Xs,2),1); 
+    C_UN = repmat([100 100 255]/255,size(Xu,2),1);
 elseif(nargin == 5)
-    C_UN = [100 100 255]/255;
+    C_SN = repmat(C_SN,size(Xs,2)/size(C_SN,1),1);
+    C_UN = repmat([100 100 255]/255,size(Xu,2),1);
+elseif(nargin == 6)
+    C_SN = repmat(C_SN,size(Xs,2)/size(C_SN,1),1);
+    C_UN = repmat(C_UN,size(Xu,2)/size(C_UN,1),1);
 end
 
 % Plot Parameters
@@ -38,12 +43,16 @@ if(d==2)
          [X(2,conn(:,1)); X(2,conn(:,2))],...
          'linewidth', lw, 'color', eC);
     % Specified Nodes
-    plot(Xs(1,:), Xs(2,:), 'o', 'linewidth', ms, 'markersize', ms, 'color', C_SN)
-    plot(Xs(1,:), Xs(2,:), 'ko', 'linewidth', bw, 'markersize', ms*2)
+    for i = 1:size(Xs,2)
+        plot(Xs(1,i), Xs(2,i), 'o', 'linewidth', ms, 'markersize', ms, 'color', C_SN(i,:))
+        plot(Xs(1,i), Xs(2,i), 'ko', 'linewidth', bw, 'markersize', ms*2)
+    end
     % Unspecified Nodes
     if(size(Xu,2) > 0)
-        plot(Xu(1,:), Xu(2,:), 'o', 'linewidth', ms, 'markersize', ms, 'color', C_UN(1,:));
-        plot(Xu(1,:), Xu(2,:), 'ko', 'linewidth', bw, 'markersize', 2*ms);
+        for i = 1:size(Xu,2)
+            plot(Xu(1,i), Xu(2,i), 'o', 'linewidth', ms, 'markersize', ms, 'color', C_UN(i,:));
+            plot(Xu(1,i), Xu(2,i), 'ko', 'linewidth', bw, 'markersize', 2*ms);
+        end
     end
     set(gca,'visible',0);
     set(gcf,'color','w');
